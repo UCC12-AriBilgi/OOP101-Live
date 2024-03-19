5d3ded0b9207c6867eacad8fc7b215ff70586744
ukaracivio <umit.karacivi@outlook.com>
2024-03-19 10:28:50 +0300

    Some changes

---
+++ P03_Inheritance/SalesRepresant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03_Inheritance
+{
+    internal class SalesRepresant : Worker
+    {
+        public string CalisanId { get; set; }
+        public int Prim { get; set; }
+        public int SatisAdedi { get; set; }
+    }
+}
---
+++ P03_Inheritance/Tutorials.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03_Inheritance
+{
+    internal class Tutorials
+    {
+        protected int TutorialID;
+        protected string TutorialName;
+
+        public void setTutorial(int pID,string pTutorialName)
+        {
+            this.TutorialID = pID;
+            this.TutorialName = pTutorialName;
+        }
+
+        public string getTutorialName()
+        {
+            return this.TutorialName;
+        }
+    }
+}
