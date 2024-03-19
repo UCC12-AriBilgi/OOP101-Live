a42829522e0617b7f9e2dde3a8253df90685eaf0
ukaracivio <umit.karacivi@outlook.com>
2024-03-19 13:45:06 +0300

    Some changes

---
+++ P05_Encapsulation/Bank.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05_Encapsulation
+{
+    public class Bank
+    {
+
+        private decimal balance; // Dışarıya kapalı içte kullandığım bir değişgen/property.. hesabımdaki miktar
+
+        // Bu sınıfın bir constructor (yapıcı) metodu olsun. Bazı ayarlar yapayım
+
+        public Bank(decimal initialBalance)
+        {
+            balance = initialBalance; // sınıftan bir nesne yaratıldığı an arka planda burası çalışacak.
+                
+        }
+
+        // Para yatırmak istiyorum diyelim
+
+        public void paraYatir(decimal miktar) // set 
+        {
+            balance = balance + miktar;
+        }
+
+        // Para çekme kısmı
+        public void paraCek(decimal miktar)
+        {
+            // bankadaki balance eğer miktar bilgisinden fazlaysa işlem yapılsın
+            if (balance >= miktar)
+            {
+                balance = balance - miktar;
+
+            }
+            else
+            {
+                Console.WriteLine("Yetersiz miktar..Lütfen kontrol ediniz....");
+            }
+        }
+
+        //Bankadaki miktarı direkt veren metot
+        public decimal getBalance() // get
+        {
+            return balance;
+        }
+    }
+}
---
+++ P05_Encapsulation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P05_Encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
