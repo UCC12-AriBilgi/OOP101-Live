44cc92dd7257034e1678a448fb3b0f3ab1b0f493
ukaracivio <umit.karacivi@outlook.com>
2024-03-19 10:45:34 +0300

    Some changes

---
+++ P03_Inheritance/Job.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03_Inheritance
+{
+    // Bazı bilgiler alabileceğim bir sınıf..sanki raporlama gibi
+    internal class Job
+    {
+        public string jobName { get; set; }
+
+        // C# da varolan class yapımızı da bir parametre olarak kullanabiliriz.
+        public void Work (Worker worker)
+        {
+            Console.WriteLine($"{worker.Ad} isimli çalışan {jobName} işinde çalışıyor..");
+        } 
+
+
+        public void RaporAl(Person person) 
+        {
+            Console.WriteLine($"{person.Ad} isimli kişi raporunu aldı....\n\n");
+        }
+    }
+}
+
---
+++ P03_Inheritance/SubTutorials.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P03_Inheritance
+{
+    internal class SubTutorials : Tutorials
+    {
+        // Burası bir alt sınıf. Burada olmayan bir özelliği ve bunlara ek olarak diğer özellik ve metodları üst sınıfta olan (Tutorials) dan kalıtım yoluyla buraya tasıdım. Üst sınıftan miras aldım.
+
+        public void DersAdiBelirle(string pNewName) 
+        {
+            TutorialName = pNewName;
+        }
+    }
+}
